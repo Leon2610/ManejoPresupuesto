142095f1cc908e5639f625ec0b572839f9102425
Leonel Leon <leonleo1996@gmail.com>
2023-05-20 15:36:01 -0600

    Insertando un Tipo de Cuenta en la Base de Datos

---
+++ ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioTiposCuentas
+    {
+        void Crear(TipoCuenta tipoCuenta);
+    }
+
+    public class RepositorioTiposCuentas : IRepositorioTiposCuentas
+    {
+        private readonly string connetionString;
+        public RepositorioTiposCuentas(IConfiguration configuration)
+        {
+            connetionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public void Crear(TipoCuenta tipoCuenta)
+        {
+            using var connection = new SqlConnection(connetionString);
+            var id = connection.QuerySingle<int>($@"INSERT INTO TiposCuentas (Nombre, UsuarioId, Orden)
+                                                    VALUES (@Nombre, @UsuarioId, 0);
+                                                    SELECT SCOPE_IDENTITY();
+                                                ", tipoCuenta);
+            tipoCuenta.Id = id;
+        }
+    }
+}
