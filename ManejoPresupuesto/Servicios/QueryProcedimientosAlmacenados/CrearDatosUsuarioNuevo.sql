563122771d6f51b3f9577e348e27252dbf4e911d
Leonel Leon <leonleo1996@gmail.com>
2023-06-02 00:04:22 -0600

    Stored Procedures CrearDatosUsuarioNuevo

---
+++ ManejoPresupuesto/Servicios/QueryProcedimientosAlmacenados/CrearDatosUsuarioNuevo.sql
@@ -0,0 +1,52 @@
+﻿-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE CrearDatosUsuarioNuevo 
+	@UsuarioId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	DECLARE @Efectivo nvarchar(50) = 'Efectivo';
+	DECLARE @CuentasDeBanco nvarchar(50) = 'Cuentas de Banco';
+	DECLARE @Tarjetas nvarchar(50) = 'Tarjetas';
+
+	INSERT INTO TiposCuentas(Nombre, UsuarioId, Orden)
+	VALUES (@Efectivo, @UsuarioId, 1),
+	(@CuentasDeBanco, @UsuarioId, 2),
+	(@Tarjetas, @UsuarioId, 3);
+
+	INSERT INTO Cuentas(Nombre, Balance, TipoCuentaId)
+	SELECT Nombre, 0, Id
+	FROM TiposCuentas
+	WHERE UsuarioId = @UsuarioId
+
+	INSERT INTO Categorias(Nombre, TipoOperacionId, UsuarioId)
+	VALUES
+	('Libros', 2, @UsuarioId),
+	('Salario', 1, @UsuarioId),
+	('Mesada', 1, @UsuarioId),
+	('Comida', 2, @UsuarioId)
+END
+GO
+
