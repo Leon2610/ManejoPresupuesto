a9e91cb3ec4714c6c004aa92f4716bfe9b0c8928
Leonel Leon <leonleo1996@gmail.com>
2023-06-02 16:28:25 -0600

    Paginación - Contando los Registros

--- ManejoPresupuesto/Controllers/CategoriasController.cs
+++ ManejoPresupuesto/Controllers/CategoriasController.cs
@@ -19,7 +19,18 @@ namespace ManejoPresupuesto.Controllers
         {
             var usuarioId = servicioUsuarios.ObtenerUsuarioId();
             var categorias = await repositorioCategorias.Obtener(usuarioId, paginacionViewModel);
-            return View(categorias);
+            var totalCategorias = await repositorioCategorias.Contar(usuarioId);
+
+            var respuestaVM = new PaginacionRespuesta<Categoria>
+            {
+                Elementos = categorias,
+                Pagina = paginacionViewModel.Pagina,
+                RecordsPorPagina = paginacionViewModel.RecordsPorPagina,
+                CantidadTotalRecors = totalCategorias,
+                BaseURL = "/categorias"
+            };
+
+            return View(respuestaVM);
         }
 
         [HttpGet]
